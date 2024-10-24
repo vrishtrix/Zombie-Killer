9e765c55bcf1e963fe50c7c985fabc171d9c4962
Vrishin Patel <vrishin_patel@outlook.com>
2024-10-24 15:14:52 +0530

    add idle, walking and death spritesheets for hero

--- src/resources.ts
+++ src/resources.ts
@@ -1,6 +1,9 @@
-import { Loader } from 'excalibur';
+import { ImageSource, Loader } from 'excalibur';
 
-const loader = new Loader();
+// Spritesheets for the hero - Idle, Walking, Death animations
+export const heroIdleLoadable = new ImageSource('./assets/hero/idle.png');
+export const heroWalkLoadable = new ImageSource('./assets/hero/walk.png');
+export const heroDeathLoadable = new ImageSource('./assets/hero/death.png');
+
+export const loader = new Loader([heroIdleLoadable]);
 loader.suppressPlayButton = true;
-
-export { loader };
